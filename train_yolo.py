9dfa71189359fe7233ac78ccab9f4dad47c71b5c
Alfaizi Ahmad Zahran <164832235+Alfascaled@users.noreply.github.com>
2025-07-02 17:39:59 +0700

    Upload Codingan

---
+++ train_yolo.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+def train_model():
+    model = YOLO("yolov8s.pt")  # atau yolov8s.pt, bebas
+    model.train(
+        data="New_Dataset/data.yaml",
+        epochs=200,
+        imgsz=768,  # kamu bisa ubah jadi 960, 1280 dll
+        batch=8,
+        name="ktp_detect",
+        workers=2,
+        exist_ok=True
+    )
+
+if __name__ == '__main__':
+    from multiprocessing import freeze_support
+    freeze_support()
+    train_model()
