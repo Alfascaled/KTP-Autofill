5cec142f2aade5d5e06e42845646871d55a4b192
Alfaizi Ahmad Zahran <bijingakak256@gmail.com>
2025-07-02 17:44:13 +0700

    Upload file

---
+++ New_Dataset/README.dataset.txt
@@ -0,0 +1,6 @@
+# KTP OCR > 2024-09-06 10:19am
+https://universe.roboflow.com/nathan-workplace/ktp-ocr-9hmdy
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ test_yolo.py
@@ -0,0 +1,30 @@
+from ultralytics import YOLO
+import cv2
+import os
+
+# Path ke model hasil training
+model_path = 'runs/detect/ktp_detect/weights/best.pt'
+
+# Path ke gambar KTP yang mau diprediksi
+image_path = 'New_Dataset/valid/images'  # ganti sama file asli kamu
+
+# Load model
+model = YOLO(model_path)
+
+# Prediksi
+results = model.predict(
+    source=image_path,
+    conf=0.55,   
+    iou = 0.45,     # confidence threshold
+    save= False,        # simpan gambar hasil deteksi
+    save_txt=True     # simpan hasil ke .txt juga
+)
+
+# Tampilin hasil di terminal
+for result in results:
+    print("===== HASIL DETEKSI =====")
+    for box in result.boxes:
+        cls = int(box.cls[0])
+        label = model.names[cls]
+        conf = box.conf[0].item()
+        print(f"{label}: {conf:.2f}")
